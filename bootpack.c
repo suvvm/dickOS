a4a7f75736a84c133ab460497f132b26246e53b0
suvvm <915269675@qq.com>
2019-10-03 19:44:07 +0800

    白色转为条纹

--- bootpack.c
+++ bootpack.c
@@ -4,7 +4,7 @@ void write_mem8(int addr, int data);
 void HariMain(void){
 	int i;
 	for(i = 0xa0000; i <= 0xaffff; i++){
-		write_mem8(i,15);	/*MOV BYTE [i],15*/
+		write_mem8(i,i & 0x0f);	/*MOV BYTE [i],15*/
 	}
 	for(;;){
 		io_hlt();
