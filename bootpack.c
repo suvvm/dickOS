61e008d3729344371d79a037ba13a1a364bd77ed
suvvm <915269675@qq.com>
2019-10-04 21:37:02 +0800

    用指针代替write_mem8操作内存

--- bootpack.c
+++ bootpack.c
@@ -1,10 +1,11 @@
 void io_hlt(void);	/*函数声明*/
-void write_mem8(int addr, int data);
 
 void HariMain(void){
 	int i;
+	char *p;
 	for(i = 0xa0000; i <= 0xaffff; i++){
-		write_mem8(i,i & 0x0f);	/*MOV BYTE [i],15*/
+		p = (char *)i;	/*用指针代替write_mem8操作内存*/
+		*p = i & 0x0f;
 	}
 	for(;;){
 		io_hlt();
