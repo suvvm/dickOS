c41d14e0c7cfc08f05c40548a0fffaaf03cc2c23
suvvm <915269675@qq.com>
2019-10-06 15:27:21 +0800

    替换指针使用

--- bootpack.c
+++ bootpack.c
@@ -3,9 +3,10 @@ void io_hlt(void);	/*函数声明*/
 void HariMain(void){
 	int i;
 	char *p;
+	p = (char *)i;	/*用指针代替write_mem8操作内存*/
 	for(i = 0xa0000; i <= 0xaffff; i++){
-		p = (char *)i;	/*用指针代替write_mem8操作内存*/
-		*p = i & 0x0f;
+		
+		*(p + i) = i & 0x0f;
 	}
 	for(;;){
 		io_hlt();
