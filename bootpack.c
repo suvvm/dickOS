c35bdc2b13b39fab910d8c321fd55385f0fe7e9e
suvvm <915269675@qq.com>
2019-10-08 22:00:40 +0800

    添加函数boxFill8用于显示矩形色块

--- bootpack.c
+++ bootpack.c
@@ -1,14 +1,25 @@
+#define COL8_000000 0
+#define COL8_FF0000 1
+#define COL8_00FF00 2
+#define COL8_FFFF00 3
+#define COL8_0000FF 4
+#define COL8_FF00FF 5
+#define COL8_00FFFF 6
+#define COL8_FFFFFF 7
+#define COL8_C6C6C6 8
+#define COL8_840000 9
+#define COL8_008400 10
+#define COL8_848400 11
+#define COL8_000084 12
+#define COL8_840084 13
+#define COL8_008484 14
+#define COL8_848484 15
+
 void io_hlt();	/*函数声明*/
 void io_cli();
 void io_out8(int port, int data);
 int io_load_eflags();
 void io_store_eflags(int eflags);
-
-
-
-
-void init_palette();
-void set_palette(int start, int end, unsigned char *rgb);
 
 void set_palette(int start, int end, unsigned char *rgb){
 	int i, eflags;
@@ -46,6 +57,15 @@ void init_palette(){
 	set_palette(0, 15, table_rgb);
 	return;
 }
+void boxFill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1){
+	int x, y;
+	for(y = y0; y <= y1; y++){
+		for(x = x0; x <= x1; x++){
+			vram[y * xsize + x] = c;
+		}
+	}
+	return;
+}
 void HariMain(){
 	int i;
 	char *p;	/*用指针代替write_mem8操作内存*/
@@ -53,9 +73,10 @@ void HariMain(){
 	init_palette();
 	
 	p = (char *) 0xa0000;
-	for(i = 0; i <= 0xffff; i++){
-		*(p + i) = i & 0x0f;
-	}
+	
+	boxFill8(p, 320, COL8_FF0000, 20, 20, 120, 120);
+	boxFill8(p, 320, COL8_00FF00, 70, 50, 170, 150);
+	boxFill8(p, 320, COL8_FF00FF, 120, 80, 220, 180);
 	for(;;){
 		io_hlt();
 	}
