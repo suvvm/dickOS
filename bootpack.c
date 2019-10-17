2e5d77cef384efbb4265d0937bfb27bfd277405c
suvvm <915269675@qq.com>
2019-10-17 11:13:29 +0800

    调用init_pic初始化可编程中断编译器

--- bootpack.c
+++ bootpack.c
@@ -1,14 +1,14 @@
 /********************************************************************************
 * @File name: bootpack.c
 * @Author: suvvm
-* @Version: 1.6.1
-* @Date: 2019-10-10
+* @Version: 1.0.2
+* @Date: 2019-10-17
 * @Description: 包含启动后要使用的功能函数
 ********************************************************************************/
 #include "bootpack.h"
 #include "desctab.c"
 #include "graphic.c"
-
+#include "interrupt.c"
 /*******************************************************
 *
 * Function name:HariMain
@@ -21,12 +21,14 @@ void HariMain(){
 	int mx, my;
 	
 	initGdtit();
+	init_pic();
 	init_palette();
+	
 	binfo = (struct BOOTINFO *) 0xff0;
-
 	mx = (binfo->scrnx - 16) / 2;
 	my = (binfo->scrny - 28 - 16) / 2;
 	init_GUI(binfo->vram, binfo->scrnx, binfo->scrny);
+	
 	putFont8_asc(binfo->vram, binfo->scrnx, 30, 30, COL8_FFFFFF, "SHOWSTRING");
 	initMouseCursor8(mcursor, COL8_008484);
 	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
