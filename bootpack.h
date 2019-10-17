61759c5fa0ee6740f44d30c24f03125436e20758
suvvm <915269675@qq.com>
2019-10-17 11:10:14 +0800

    添加关于色号 描述符 PIC端口的宏定义

--- bootpack.h
+++ bootpack.h
@@ -1,13 +1,55 @@
 /********************************************************************************
 * @File name: bootpack.c
 * @Author: suvvm
-* @Version: 1.0.1
-* @Date: 2019-10-13
+* @Version: 1.0.2
+* @Date: 2019-10-17
 * @Description: 函数声明
 ********************************************************************************/
 
 #ifndef BOOTPACK_H
 #define BOOTPACK_H
+
+/*色号信息*/
+#define COL8_000000		0
+#define COL8_FF0000		1
+#define COL8_00FF00		2
+#define COL8_FFFF00		3
+#define COL8_0000FF		4
+#define COL8_FF00FF		5
+#define COL8_00FFFF		6
+#define COL8_FFFFFF		7
+#define COL8_C6C6C6		8
+#define COL8_840000		9
+#define COL8_008400		10
+#define COL8_848400		11
+#define COL8_000084		12
+#define COL8_840084		13
+#define COL8_008484		14
+#define COL8_848484		15
+
+/*描述符信息*/
+#define ADR_IDT			0x0026f800
+#define LIMIT_IDT		0x000007ff
+#define ADR_GDT			0x00270000
+#define LIMIT_GDT		0x0000ffff
+#define ADR_BOTPAK		0x00280000
+#define LIMIT_BOTPAK	0x0007ffff
+#define AR_DATA32_RW	0x4092
+#define AR_CODE32_ER	0x409a
+
+/*PIC端口信息*/
+#define PIC0_ICW1		0x0020	// 写主PIC ICW1的端口地址
+#define PIC0_OCW2		0x0020	// 写主PIC OCW2的端口地址
+#define PIC0_IMR		0x0021	// 写主PIC 由OCW1写IMR寄存器的端口地址
+#define PIC0_ICW2		0x0021	// 写主PIC ICW2的端口地址
+#define PIC0_ICW3		0x0021	// 写主PIC ICW3的端口地址
+#define PIC0_ICW4		0x0021	// 写主PIC ICW4的端口地址
+#define PIC1_ICW1		0x00a0	// 写从PIC ICW1的端口地址
+#define PIC1_OCW2		0x00a0	// 写从PIC OCW2的端口地址
+#define PIC1_IMR		0x00a1	// 写从PIC 由OCW1写IMR寄存器的端口地址
+#define PIC1_ICW2		0x00a1	// 写从PIC ICW2的端口地址
+#define PIC1_ICW3		0x00a1	// 写从PIC ICW3的端口地址
+#define PIC1_ICW4		0x00a1	// 写从PIC ICW4的端口地址
 
 /*启动信息*/
 struct BOOTINFO{	
@@ -67,5 +109,8 @@ void putblock8_8(char *vram, int vxsize, int pxsize,
 void initGdtit();
 void setSegmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
 void setGatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
+
+/*interrupt.c函数声明*/
+void init_pic();
 
 #endif // BOOTPACK_H
