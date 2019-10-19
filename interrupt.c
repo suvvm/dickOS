855249dcb46720d68d9d76dd235ccf67a58cb679
suvvm <915269675@qq.com>
2019-10-19 22:04:45 +0800

    添加中断处理函数interruptHandler21 interruptHandler2c interruptHandler21

--- interrupt.c
+++ interrupt.c
@@ -1,10 +1,12 @@
 /********************************************************************************
 * @File name: interrupt.c
 * @Author: suvvm
-* @Version: 1.0.1
-* @Date: 2019-10-16
+* @Version: 1.0.2
+* @Date: 2019-10-19
 * @Description: 中断操作
 ********************************************************************************/
+
+#include "bootpack.h"
 
 /*******************************************************
 *
@@ -28,4 +30,61 @@ void init_pic(){
 	
 	io_out8(PIC0_IMR, 0xfb);	/* 11111011 PIC0_ICW1以外全部禁止*/
 	io_out8(PIC1_IMR, 0xff);	/* 11111111 禁止所有中断*/
+}
+
+/*******************************************************
+*
+* Function name:interruptHandler21
+* Description: 接收来自IRQ2 PS/2键盘的中断并显示提示信息
+* Parameter:
+* 	@esp	接收指针的值
+*
+*******************************************************/
+void interruptHandler21(int *esp){
+	// 获取启动信息
+	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
+	// 显示信息背景
+	boxFill8(binfo->vram, binfo->scrnx, COL8_000000, 0, 0, 32 * 8 - 1, 15);
+	// 显示提示信息
+	putFont8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, "INT 21 (IRQ-1) : PS/2 keyboard");
+	
+	for(;;){
+		io_hlt();
+	}
+}
+
+/*******************************************************
+*
+* Function name:interruptHandler27
+* Description: 接收来自IRQ7 PIC初始化中断 
+*	针对来自PIC0的不完全中断，在某些机型中等由于芯片组的情况PIC的初始化时会发送IRQ7中断，
+*	如果不对该中断处理程序执行STI（设置中断标指位），操作系统会启动失败
+* Parameter:
+* 	@esp	接收指针的值
+*
+*******************************************************/
+void interruptHandler27(int *esp){
+	io_out8(PIC0_OCW2, 0x67);
+	return;
+}
+
+/*******************************************************
+*
+* Function name:interruptHandler2c
+* Description: 接收来自IRQ12 鼠标的中断并显示提示信息
+* Parameter:
+* 	@esp	接收指针的值
+*
+*******************************************************/
+void interruptHandler2c(int *esp){
+	// 获取启动信息
+	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
+	// 显示信息背景
+	boxFill8(binfo->vram, binfo->scrnx, COL8_000000, 0, 0, 32 * 8 - 1, 15);
+	// 显示提示信息
+	putFont8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, "INT 2C (IRQ-12) : PS/2 mouse");
+	
+	for (;;) {
+		io_hlt();
+	}
 }
