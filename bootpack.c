cd739e36d7921c8f14097de8600efb82213ebdc6
suvvm <915269675@qq.com>
2019-10-21 19:06:48 +0800

    添加对缓冲区数据显示的处理

--- bootpack.c
+++ bootpack.c
@@ -1,8 +1,8 @@
 /********************************************************************************
 * @File name: bootpack.c
 * @Author: suvvm
-* @Version: 1.0.3
-* @Date: 2019-10-19
+* @Version: 1.0.4
+* @Date: 2019-10-21
 * @Description: 包含启动后要使用的功能函数
 ********************************************************************************/
 #include "bootpack.h"
@@ -18,7 +18,7 @@
 void HariMain(){
 	struct BOOTINFO *binfo;
 	char mcursor[256], s[40];	// mcursor鼠标信息 s保存要输出的变量信息
-	int mx, my;
+	int mx, my, keybufval;
 	
 	initGdtit();	// 初始化GDT IDT
 	init_pic();	// 初始化可编程中断控制器
@@ -43,8 +43,18 @@ void HariMain(){
 	io_out8(PIC0_IMR, 0xf9); // 主PIC IRQ1（键盘）与IRQ2（从PIC）不被屏蔽(11111001)
 	io_out8(PIC1_IMR, 0xef); // 从PIC IRQ12（鼠标）不被控制(11101111)
 	
-	//进入hlt状态 
+	//处理键盘中断与进入hlt
 	for(;;){
-		io_hlt();
+		io_cli();
+		if (keybuf.flag == 0) {
+			io_stihlt();
+		} else {
+			keybufval = keybuf.data;
+			keybuf.flag = 0;
+			io_sti();
+			sprintf(s, "%02X", keybufval);
+			boxFill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
+			putFont8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+		}
 	}
 }
