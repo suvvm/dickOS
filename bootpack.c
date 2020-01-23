767421123a0941b05c2b715843a6d735cdfa1f45
suvvm <915269675@qq.com>
2020-01-23 21:53:05 +0800

    添加显示窗口函数

--- bootpack.c
+++ bootpack.c
@@ -1,7 +1,7 @@
 /********************************************************************************
 * @File name: bootpack.c
 * @Author: suvvm
-* @Version: 0.1.9
+* @Version: 0.2.1
 * @Date: 2020-01-23
 * @Description: 包含启动后要使用的功能函数
 ********************************************************************************/
@@ -12,6 +12,66 @@
 #include "mouse.c"
 #include "memory.c"
 #include "sheet.c"
+
+
+/*******************************************************
+*
+* Function name: makeWindow
+* Description: 创建窗口
+* Parameter:
+*	@buf	窗口图像缓冲区	unsigned char *
+*	@width	窗口宽度		int
+*	@height	窗口高度		int
+*	@title	窗口标题		char *
+*
+**********************************************************/
+void makeWindow(unsigned char *buf, int width, int height, char *title) {
+	static char closeBtn[14][16] = {	// 关闭按钮
+		"OOOOOOOOOOOOOOO@",
+		"OQQQQQQQQQQQQQ$@",
+		"OQQQQQQQQQQQQQ$@",
+		"OQQQ@@QQQQ@@QQ$@",
+		"OQQQQ@@QQ@@QQQ$@",
+		"OQQQQQ@@@@QQQQ$@",
+		"OQQQQQQ@@QQQQQ$@",
+		"OQQQQQ@@@@QQQQ$@",
+		"OQQQQ@@QQ@@QQQ$@",
+		"OQQQ@@QQQQ@@QQ$@",
+		"OQQQQQQQQQQQQQ$@",
+		"OQQQQQQQQQQQQQ$@",
+		"O$$$$$$$$$$$$$$@",
+		"@@@@@@@@@@@@@@@@"
+	};
+	int x, y;
+	char c;
+	// 绘制窗口（坐标都为相对坐标，起始(0,0)）
+	boxFill8(buf, width, COL8_C6C6C6, 0, 0, width - 1, 0);	// 亮灰色 (0,0)~(width-1,0)顶部横线
+	boxFill8(buf, width, COL8_FFFFFF, 1, 1, width - 2, 1);	// 白色 (1,1)~(width-2,1)顶部横线
+	boxFill8(buf, width, COL8_C6C6C6, 0, 0, 0, height - 1);	// 亮灰色 (0,0)~(0,height-1)左侧竖线
+	boxFill8(buf, width, COL8_FFFFFF, 1, 1, 1, height - 2);	// 白色 (1,1)~(1,height-2)左侧竖线
+	boxFill8(buf, width, COL8_848484, width - 2, 1, width - 2, height - 2);	//暗灰色 (width - 2,1)~(width - 2, height - 2)一条右侧竖线
+	boxFill8(buf, width, COL8_000000, width - 1, 0, width - 1, height - 1);	// 黑色 (width - 1,0)~(width - 1, height - 1)一条右侧竖线
+	boxFill8(buf, width, COL8_C6C6C6, 2, 2, width - 3, height - 3);	// 亮灰色 (2,2)~(width - 3,height - 3) 中心矩形
+	boxFill8(buf, width, COL8_000084, 3, 3, width - 4, 20);	// 暗青色(3,3)~(width - 4, 20)顶部矩形
+	boxFill8(buf, width, COL8_848484, 1, height - 2, width - 2, height - 2);	// 暗灰色(1,height-2)~(width-2,height-2)底部横线
+	boxFill8(buf, width, COL8_000000, 0, height - 1, width - 1, height - 1);	// 黑色(0,height-1)~(width-1,height-1)底部横线
+	putFont8_asc(buf, width, 24, 4, COL8_FFFFFF, title);
+	for (y = 0; y < 14; y++) {
+		for (x = 0; x < 16; x++) {
+			c = closeBtn[y][x];
+			if (c == '@') {
+				c = COL8_000000;
+			} else if (c == '$') {
+				c = COL8_848484;
+			} else if (c == 'Q') {
+				c = COL8_C6C6C6;
+			} else {
+				c = COL8_FFFFFF;
+			}
+			buf[(5 + y) * width + (width - 21 + x)] = c; 
+		}
+	}
+}
 
 /*******************************************************
 *
@@ -27,8 +87,8 @@ void Main(){
 	unsigned int memtotal;
 	struct MEMSEGTABLE *memsegtable = (struct MEMSEGTABLE *) MEMSEG_ADDR;	// 内存段表指针
 	struct SHTCTL *shtctl;	// 图层控制块指针
-	struct SHEET *sheetBack, *sheetMouse;	// 背景图层 鼠标图层
-	unsigned char *bufBack, bufMouse[256];
+	struct SHEET *sheetBack, *sheetMouse, *sheetWin;	// 背景图层 鼠标图层 窗口图层
+	unsigned char *bufBack, bufMouse[256], *bufWin;	// 背景图像缓冲区 鼠标图像缓冲区 窗口图像缓冲区
 	
 	binfo = (struct BOOTINFO *) ADR_BOOTINFO;	// 获取启动信息
 	
@@ -55,26 +115,32 @@ void Main(){
 	shtctl = shtctlInit(memsegtable, binfo->vram, binfo->scrnx, binfo->scrny);
 	sheetBack = sheetAlloc(shtctl);
 	sheetMouse = sheetAlloc(shtctl);
+	sheetWin = sheetAlloc(shtctl);
 	bufBack = (unsigned char *) memsegAlloc4K(memsegtable, binfo->scrnx * binfo->scrny);	// 以4KB为单位为背景分配内存
+	bufWin = (unsigned char *) memsegAlloc4K(memsegtable, 160 * 68);
 	sheetSetbuf(sheetBack, bufBack, binfo->scrnx, binfo->scrny, -1);	// 设置背景图层缓冲区，背景不需要透明色设为-1
-	
 	sheetSetbuf(sheetMouse, bufMouse, 16, 16, COL8_008484);	// 设置鼠标图层缓冲区与透明色
+	sheetSetbuf(sheetWin, bufWin, 160, 68, -1);	// 设置窗口图层缓冲区 无透明色
 	init_GUI(bufBack, binfo->scrnx, binfo->scrny);	// 初始化GUI至bufBack
 	initMouseCursor8(bufMouse, COL8_008484);	// 初始化鼠标至bufMouse
-	
+	makeWindow(bufWin, 160, 68, "window");	// 显示窗口
+	putFont8_asc(bufWin, 160, 24, 28, COL8_000000, "Welcome to");
+	putFont8_asc(bufWin, 160, 24, 44, COL8_000000, "DickOS");
 	sheetSlide(sheetBack, 0, 0);	// 背景图层起始坐标(0,0)
 	// 初始鼠标坐标为屏幕正中
 	mx = (binfo->scrnx - 16) / 2;	// 鼠标x轴位置
 	my = (binfo->scrny - 28 - 16) / 2;	// 鼠标y轴位置
 	sheetSlide(sheetMouse, mx, my);	// 将鼠标图层滑动至对应位置
+	sheetSlide(sheetWin, 80, 80);	// 将窗口图层滑动至目标位置
 	sheetUpdown(sheetBack, 0);	// 将背景图层置于索引0
-	sheetUpdown(sheetMouse, 1);	// 将鼠标图层置于背景层上方索引1
+	sheetUpdown(sheetWin, 1);	// 将窗口图层置于背景之上鼠标之下
+	sheetUpdown(sheetMouse, 2);	// 将鼠标图层置于背景层上方索引1
 	putFont8_asc(bufBack, binfo->scrnx, 30, 32, COL8_FFFFFF, "DickOS");	// 将DickOS写入背景层
 	sprintf(s, "(%d, %d)", mx, my);	// 将鼠标位置存入s
 	putFont8_asc(bufBack, binfo->scrnx, 0, 0, COL8_FFFFFF, s);	// 将s写入背景图层
 	sprintf(s, "memory %dMB free : %dKB", memtest(0x00400000, 0xbfffffff) / (1024 * 1024), memsegTotal(memsegtable) / 1024);	// 将内存信息存入s
 	putFont8_asc(bufBack, binfo->scrnx, 0, 64, COL8_FFFFFF, s);	// 将s写入背景图层
-	sheetRefresh(sheetBack, 0, 0, binfo->scrnx, 64 + 16 + 16);	// 刷新图层
+	sheetRefresh(sheetBack, 0, 0, binfo->scrnx, 64 + 16 + 16 + 76);	// 刷新图层
 	
 	//处理键盘与鼠标中断与进入hlt
 	for(;;){
