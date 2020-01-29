b46f0a3d15b23c80d8f7afacb703e40a58001663
suvvm <915269675@qq.com>
2020-01-29 18:24:14 +0800

    添加函数sheetRefreshMap以刷新图层控制块map中的记录 修改函数sheetRefrestSub并修改相关调用

--- sheet.c
+++ sheet.c
@@ -1,8 +1,8 @@
 /********************************************************************************
 * @File name: sheet.c
 * @Author: suvvm
-* @Version: 0.0.5
-* @Date: 2020-01-25
+* @Version: 0.0.6
+* @Date: 2020-01-29
 * @Description: 定义图层相关函数
 ********************************************************************************/
 #include "bootpack.h"
@@ -25,6 +25,11 @@ struct SHTCTL *shtctlInit(struct MEMSEGTABLE *memsegtable, unsigned char *vram, 
 	int i;
 	shtctl = (struct SHTCTL *) memsegAlloc4K(memsegtable, sizeof(struct SHTCTL));	// 计算SHTCTL所需内存并以4KB为单位进行分配
 	if (shtctl == 0) {	// 内存分配失败
+		return shtctl;
+	}
+	shtctl->map = (unsigned char *) memsegAlloc4K(memsegtable, xSize * ySize);	// 为map分配内存空间
+	if (shtctl->map == 0) {	// 分配失败
+		memsegFree4K(memsegtable, (int)shtctl, sizeof(struct SHTCTL));
 		return shtctl;
 	}
 	shtctl->vram = vram;
@@ -83,6 +88,68 @@ void sheetSetbuf(struct SHEET *sheet, unsigned char *buf, int width, int height,
 
 /*******************************************************
 *
+* Function name: sheetRefreshMap
+* Description: 刷新map中点归属的记录
+* Parameter:
+*	@shtctl	图层控制块指针				struct SHTCTL *
+*	@startX	欲刷新区域的x轴起始位置		int
+*	@startY	欲刷新区域的y轴起始位置		int
+*	@endX	欲刷新区域的x轴截止位置		int
+*	@endY	欲刷新区域的y轴截止位置		int
+*	@startIndex	欲刷新的指定开始索引	int
+*
+**********************************************************/
+void sheetRefreshMap(struct SHTCTL *shtctl, int startX, int startY, int endX, int endY, int startIndex) {	
+	int i, sheetX, sheetY, locationX, locationY, relativeStartX, relativeStartY, relativeEndX, relativeEndY;
+	unsigned char *buf, sid, *map = shtctl->map;
+	struct SHEET *sheet;
+	if (startX < 0) {	// 欲刷新区域x轴起始位置超边界修正
+		startX = 0;
+	}
+	if (startY < 0) {	// 欲刷新区域y轴起始位置超边界修正
+		startY = 0;
+	}
+	if (endX > shtctl->xSize) {	// 欲刷新区域x轴截止位置超边界修正
+		endX = shtctl->xSize;
+	}
+	if (endY > shtctl->ySize) {	// 欲刷新区域y轴截止位置超边界修正
+		endY = shtctl->ySize;
+	}
+	for (i = startIndex; i <= shtctl->top; i++) {
+		sheet = shtctl->sheetsAcs[i];
+		sid = sheet - shtctl->sheets;	// 使用当前图层地址减去图层存储起始地址的结果（偏移量）作为图层id
+		buf = sheet->buf;
+		// 获取要刷新的区域在图层中的对应位置
+		relativeStartX = startX - sheet->locationX;	// 在图层中欲刷新区域对应的x轴起始位置
+		relativeStartY = startY - sheet->locationY;	// 在图层中欲刷新区域对应的y轴起始位置
+		relativeEndX = endX - sheet->locationX;	// 在图层中欲刷新区域对应的x轴截止位置
+		relativeEndY = endY - sheet->locationY;	// 在图层中欲刷新区域对应的x轴截止位置
+		if (relativeStartX < 0) {	// 修正溢出部分
+			relativeStartX = 0;
+		}
+		if (relativeStartY < 0) {
+			relativeStartY = 0;
+		}
+		if (relativeEndX > sheet->width) {
+			relativeEndX = sheet->width;
+		}
+		if (relativeEndY > sheet->height) {
+			relativeEndY = sheet->height;
+		}
+		for (sheetY = relativeStartY; sheetY < relativeEndY; sheetY++) {
+			locationY = sheet->locationY + sheetY;	// 找到图像在vram y轴映射的位置
+			for (sheetX = relativeStartX; sheetX < relativeEndX; sheetX++) {
+				locationX = sheet->locationX + sheetX;	// 找到图像在vram x轴映射的位置
+				if (buf[sheetY * sheet->width + sheetX] != sheet->colInvNum) {	// 对应位置不为图层透明色
+					map[locationY * shtctl->xSize + locationX] = sid;	// 将对应点归属记为对应图层
+				}
+			}
+		}
+	}
+}
+
+/*******************************************************
+*
 * Function name: sheetUpdown
 * Description: 设置图层索引
 * Parameter:
@@ -108,6 +175,7 @@ void sheetUpdown(struct SHEET *sheet, int index) {
 				shtctl->sheetsAcs[i]->index = i;
 			}
 			shtctl->sheetsAcs[index] = sheet;
+			sheetRefreshMap(sheet->shtctl, sheet->locationX, sheet->locationY, sheet->locationX + sheet->width, sheet->locationY + sheet->height, index + 1);	// 刷新map
 			sheetRefreshSub(sheet->shtctl, sheet->locationX, sheet->locationY, sheet->locationX + sheet->width, sheet->locationY + sheet->height, index + 1);	// 重新绘制画面
 		} else {	// 隐藏
 			if(shtctl->top > old) {	// 最高索引大于之前的索引值（图层不在最高层）
@@ -117,6 +185,7 @@ void sheetUpdown(struct SHEET *sheet, int index) {
 				}
 			}
 			shtctl->top--;
+			sheetRefreshMap(sheet->shtctl, sheet->locationX, sheet->locationY, sheet->locationX + sheet->width, sheet->locationY + sheet->height, 0);	// 刷新map
 			sheetRefreshSub(sheet->shtctl, sheet->locationX, sheet->locationY, sheet->locationX + sheet->width, sheet->locationY + sheet->height, 0);	// 重新绘制画面
 		}
 	} else if (old < index) {	// 若新的索引值大于之前的索引值 将更新的图层提升至目标位置
@@ -134,6 +203,7 @@ void sheetUpdown(struct SHEET *sheet, int index) {
 			shtctl->sheetsAcs[index] = sheet;	// 将图层放至对应位置
 			shtctl->top++;
 		}
+		sheetRefreshMap(sheet->shtctl, sheet->locationX, sheet->locationY, sheet->locationX + sheet->width, sheet->locationY + sheet->height, index);
 		sheetRefreshSub(sheet->shtctl, sheet->locationX, sheet->locationY, sheet->locationX + sheet->width, sheet->locationY + sheet->height, index);
 	}
 }
@@ -151,26 +221,8 @@ void sheetUpdown(struct SHEET *sheet, int index) {
 *
 **********************************************************/
 void sheetRefresh(struct SHEET *sheet, int startX, int startY, int endX, int endY) {
-	/* sheetRefreshAll
-	int i, sheetX, sheetY, locationX, locationY;
-	unsigned char *buf, c, *vram = shtctl->vram;
-	struct SHEET *sheet;
-	for (i = 0; i <= shtctl->top; i++) {	// 自底向上遍历所有图层
-		sheet = shtctl->sheetsAcs[i];
-		buf = sheet->buf;
-		for(sheetY = 0; sheetY < sheet->height; sheetY++) {
-			locationY = sheet->locationY + sheetY;
-			for(sheetX = 0; sheetX < sheet->width; sheetX++) {
-				locationX = sheet->locationX + sheetX;
-				c = buf[sheetY * sheet->width + sheetX];	// 找到目标颜色
-				if (c != sheet->colInvNum) {	// 当前要绘制的颜色不是该图层透明色
-					vram[locationY * shtctl->xSize + locationX] = c;
-				}
-			}
-		}
-	}
-	*/
 	if (sheet->index >= 0) {	// 图层未被隐藏
+		// 图层上下级索引没有改变 不需要刷新map
 		sheetRefreshSub(sheet->shtctl, sheet->locationX + startX, sheet->locationY + startY, sheet->locationX + endX, sheet->locationY + endY, sheet->index);	// 刷新vram指定位置
 	}
 }
@@ -185,7 +237,7 @@ void sheetRefresh(struct SHEET *sheet, int startX, int startY, int endX, int end
 *	@startY		欲刷新区域y轴起始坐标	int
 *	@endX		欲刷新区域的宽度		int
 *	@endY		欲刷新区域的高度		int
-*	@startIndex	要与刷新的指定开始索引	int
+*	@startIndex	欲刷新的指定开始索引	int
 *
 **********************************************************/
 void sheetRefreshSub(struct SHTCTL *shtctl, int startX, int startY, int endX, int endY, int startIndex) {
@@ -211,7 +263,7 @@ void sheetRefreshSub(struct SHTCTL *shtctl, int startX, int startY, int endX, in
 	}
 	*/
 	int i, sheetX, sheetY, locationX, locationY, relativeStartX, relativeStartY, relativeEndX, relativeEndY;
-	unsigned char *buf, c, *vram = shtctl->vram;
+	unsigned char *buf, *vram = shtctl->vram, *map = shtctl->map, sid;
 	struct SHEET *sheet;
 	if (startX < 0) {
 		startX = 0;
@@ -227,6 +279,7 @@ void sheetRefreshSub(struct SHTCTL *shtctl, int startX, int startY, int endX, in
 	}
 	for (i = startIndex; i <= shtctl->top; i++) {	// 只刷新指定索引及以上的图层
 		sheet = shtctl->sheetsAcs[i];
+		sid = sheet - shtctl->sheets;
 		buf = sheet->buf;
 		// 获取要刷新的区域在图层中的对应位置
 		relativeStartX = startX - sheet->locationX;
@@ -249,9 +302,8 @@ void sheetRefreshSub(struct SHTCTL *shtctl, int startX, int startY, int endX, in
 			locationY = sheet->locationY + sheetY;	// 找到图像在vram y轴映射的位置
 			for (sheetX = relativeStartX; sheetX < relativeEndX; sheetX++) {
 				locationX = sheet->locationX + sheetX;	// 找到图像在vram x轴映射的位置
-				c = buf[sheetY * sheet->width + sheetX];
-				if (c != sheet->colInvNum) {	// c不为图层透明色
-					vram[locationY * shtctl->xSize + locationX] = c;
+				if (map[locationY * shtctl->xSize + locationX] == sid) {	// 欲刷新位置当前属于该图层
+					vram[locationY * shtctl->xSize + locationX] = buf[sheetY * sheet->width + sheetX];
 				}
 			}
 		}
@@ -273,10 +325,10 @@ void sheetSlide(struct SHEET *sheet, int locationX, int locationY) {
 	sheet->locationX = locationX;
 	sheet->locationY = locationY;
 	if (sheet->index >= 0) {	// 图层未被隐藏则重新绘制画面
+		sheetRefreshMap(sheet->shtctl, oldLocationX, oldLocationY, oldLocationX + sheet->width, oldLocationY + sheet->height, 0);	// map刷新移动前区域
+		sheetRefreshMap(sheet->shtctl, locationX, locationY, locationX + sheet->width, locationY + sheet->height, sheet->index);	// map刷新移动目标区域
 		sheetRefreshSub(sheet->shtctl, oldLocationX, oldLocationY, oldLocationX + sheet->width, oldLocationY + sheet->height, 0);	// 刷新移动前区域
 		sheetRefreshSub(sheet->shtctl, locationX, locationY, locationX + sheet->width, locationY + sheet->height, sheet->index);	// 刷新移动目标区域
-		
-		//sheetRefresh(shtctl);
 	}
 }
 
