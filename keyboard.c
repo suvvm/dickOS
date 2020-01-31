8edd5fa07d0d2e98c16731c38735e8c5a72de731
suvvm <915269675@qq.com>
2020-01-31 20:48:46 +0800

    添加缓冲区指针keybuf与缓冲区数据起点值keyData并修改键盘控制电路初始化c函数

--- keyboard.c
+++ keyboard.c
@@ -1,3 +1,5 @@
+#ifndef KEYBOARD_C
+#define KEYBOARD_C
 /********************************************************************************
 * @File name: keyboard.c
 * @Author: suvvm
@@ -7,6 +9,10 @@
 ********************************************************************************/
 
 #include "bootpack.h"
+
+struct QUEUE *keybuf;
+
+int keyData0;	// 键盘缓冲区数据起点值
 
 /*******************************************************
 *
@@ -30,11 +36,18 @@ void waitKeyboardControllerReady(){
 * Description: 初始化键盘控制电路模式 
 *	等待键盘控制电路就绪后向命令接收端口发送更改模式的指令
 *	再次等待键盘控制电路就绪后向模式控制端口发送要更改的模式
+* Parameter: 
+*	@q		缓冲区队列指针		struct QUEUE *
+*	@data0	设置keyData0的值	int
 *
 *******************************************************/
-void initKeyboard(){
+void initKeyboard(struct QUEUE *q, int data0){
+	keybuf = q;
+	keyData0 = data0;
 	waitKeyboardControllerReady();
 	io_out8(PORT_KEYCMD, KEYCMD_WRITE_MODE);
 	waitKeyboardControllerReady();
 	io_out8(PORT_KEYDAT, KBC_MODE);
 }
+
+#endif // KEYBOARD_C
