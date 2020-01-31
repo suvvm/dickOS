99397b64ad94d1280db335d69db045e107b57d3a
suvvm <915269675@qq.com>
2020-01-31 22:12:18 +0800

    初始化定时器时同时设置一个超时时间0xffffffff的哨兵

--- timer.c
+++ timer.c
@@ -3,7 +3,7 @@
 /********************************************************************************
 * @File name: timer.c
 * @Author: suvvm
-* @Version: 0.0.5
+* @Version: 0.0.6
 * @Date: 2020-01-31
 * @Description: 定义定时器相关函数
 ********************************************************************************/
@@ -18,16 +18,21 @@ struct TIMERCTL timerctl;
 *
 **********************************************************/
 void initPit() {
+	int i;
+	struct TIMER *t;
 	io_out8(PIT_CTRL, 0x34);
 	io_out8(PIT_CNT0, 0x9c);
 	io_out8(PIT_CNT0, 0x2e);
 	timerctl.count = 0;	// /定时器中断次数初始化为0
-	timerctl.next = 0xffffffff;	// 初始没有正在运行的定时器
-	timerctl.nowUsing = 0;
-	int i;
 	for (i = 0; i < MAX_TIMER; i++) {
 		timerctl.timer[i].status = 0;	// 初始化所有定时器未使用
 	}
+	t = timerAlloc();	// 先取得一个可用定时器
+	t->timeout = 0xffffffff;	//  预留哨兵
+	t->status = TIMER_USING;
+	t->next = 0;
+	timerctl.timerHead = t;	// 只有哨兵，所以哨兵为链表头结点
+	timerctl.next = 0xffffffff;	// 下一个超时时间为哨兵超时时间
 }
 
 /********************************************************
@@ -92,15 +97,7 @@ void timerSetTime(struct TIMER *timer, unsigned int timeout) {
 	timer->status = TIMER_USING;	// 定时器正在运行
 	eflags = io_load_eflags();	// 保存eflags状态
 	io_cli();	// 关中断
-	timerctl.nowUsing++;	// 当前运行寄存器数量加一
-	if (timerctl.nowUsing == 1) {	// 处于运行态的定时器只有新加的这一个
-		timerctl.timerHead = timer;
-		timer->next = 0;	// 链表只有头结点
-		timerctl.next = timer->timeout;	// 更新下一次超时的时限
-		io_store_eflags(eflags);	// 恢复eflags（恢复中断位开放中断）
-		return;
-	}
-	t = timerctl.timerHead;	// t为先前运行寄存器链表头节点
+	t = timerctl.timerHead;	// 由于有哨兵，所以一定存在链表头 不会出现只有新的定时器一个和插入链表尾的情况（哨兵总是在最后）
 	if (timer->timeout <= t->timeout) {	// 新的定时器超时时间比头结点短 头插
 		timerctl.timerHead = timer;
 		timer->next = t;
@@ -113,8 +110,7 @@ void timerSetTime(struct TIMER *timer, unsigned int timeout) {
 		t = t->next;
 		if (t == 0) {	// 链表尾
 			break;
-		}
-		
+		}		
 		if (timer->timeout <= t->timeout) {	// 找到位置，插入s与t之间
 			s->next = timer;
 			timer->next = t;
@@ -122,10 +118,6 @@ void timerSetTime(struct TIMER *timer, unsigned int timeout) {
 			return;
 		}
 	}
-	// 插入链表尾
-	// s当前指向链表尾
-	s->next = timer;
-	timer->next = 0;	// timer为新的链表尾
 	io_store_eflags(eflags);	// 恢复eflags（恢复中断位开放中断）
 }
 
