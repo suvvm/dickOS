fcdd815f37c2f0ffc52520f0fa586621a2f85304
suvvm <915269675@qq.com>
2020-01-31 14:26:18 +0800

    添加定时器相关函数以支持多定时器

--- timer.c
+++ timer.c
@@ -1,8 +1,8 @@
 /********************************************************************************
 * @File name: timer.c
 * @Author: suvvm
-* @Version: 0.0.2
-* @Date: 2020-01-30
+* @Version: 0.0.3
+* @Date: 2020-01-31
 * @Description: 定义定时器相关函数
 ********************************************************************************/
 #include "bootpack.h"
@@ -19,10 +19,73 @@ void initPit() {
 	io_out8(PIT_CTRL, 0x34);
 	io_out8(PIT_CNT0, 0x9c);
 	io_out8(PIT_CNT0, 0x2e);
-	timerctl.count = 0;	// 计时器中断次数初始化为0
-	timerctl.timeout = 0;	// 超时时限初始化为0
-	
+	timerctl.count = 0;	// /定时器中断次数初始化为0
+	int i;
+	for (i = 0; i < MAX_TIMER; i++) {
+		timerctl.timer[i].status = 0;	// 初始化所有定时器未使用
+	}
 }
+
+/********************************************************
+*
+* Function name:	timerAlloc
+* Description:	取得新的未使用的定时器
+* Return:
+*	成功返回指向对应定时器的指针，失败返回0
+*
+**********************************************************/
+struct TIMER *timerAlloc() {
+	int i;
+	for (i = 0; i < MAX_TIMER; i++) {
+		if (timerctl.timer[i].status == 0) {
+			timerctl.timer[i].status = TIMER_ALLOC;
+			return &timerctl.timer[i];
+		}
+	}
+	return 0;
+}
+
+/********************************************************
+*
+* Function name:	timerFree
+* Description:	释放目标定时器
+* Parameter:
+*	@timer	欲释放定时器指针	struct TIMER *
+*
+**********************************************************/
+void timerFree(struct TIMER *timer) {
+	timer->status = 0;	// 标记为未使用
+}
+
+/********************************************************
+*
+* Function name:	timerInit
+* Description:	初始化定时器
+* Parameter:
+*	@timer	欲初始化定时器指针	struct TIMER *
+*	@queue	定时器缓冲区队列	struct QUEUE *
+*	@data	定时器超时信息		unsigned char
+*
+**********************************************************/
+void timerInit(struct TIMER *timer, struct QUEUE *queue, unsigned char data) {
+	timer->queue = queue;
+	timer->data = data;
+}
+
+/********************************************************
+*
+* Function name:	timerSetTime
+* Description: 设置定时器超时时限
+* Parameter:
+*	@timer		定时器指针			struct TIMER *
+* 	@timeout	定时器超时时限		unsigned int
+*
+**********************************************************/
+void timerSetTime(struct TIMER *timer, unsigned int timeout) {
+	timer->timeout = timeout;
+	timer->status = TIMER_USING;	// 定时器正在运行
+}
+
 
 /********************************************************
 *
@@ -33,33 +96,16 @@ void initPit() {
 *
 **********************************************************/
 void interruptHandler20(int *esp) {
+	int i;
 	io_out8(PIC0_OCW2, 0x60);	// 将IRQ-0信号接收完毕的消息通知给PIC
 	timerctl.count++;	// 计时器控制块中的计数
-	if (timerctl.timeout > 0) {	// 如果已经设置了超时时限
-		timerctl.timeout--;
-		if (timerctl.timeout == 0) {	// 若超时就将超时数据写入缓冲队列中
-			QueuePush(timerctl.queue, timerctl.data);
-		}
+	for (i = 0; i < MAX_TIMER; i++) {
+		if (timerctl.timer[i].status == TIMER_USING) {	// 如果定时器正在运行
+			timerctl.timer[i].timeout--;
+			if (timerctl.timer[i].timeout == 0) {	// 若超时就将超时数据写入缓冲队列中
+				timerctl.timer[i].status = TIMER_ALLOC;	// 定时器设为已分配可使用态
+				QueuePush(timerctl.timer[i].queue, timerctl.timer[i].data);	// 对应超时信息入队对应缓冲区队列
+			}
+		}	
 	}
-}
-
-/********************************************************
-*
-* Function name:	settimer
-* Description: 设置定时器信息
-* Parameter:
-* 	@timeout	定时器超时时限		unsigned int
-*	@queue		定时器缓冲区队列	struct QUEUE *
-*	@data		定时器超时信息		unsigned char
-*
-**********************************************************/
-void setTimer(unsigned int timeout, struct QUEUE *queue, unsigned char data) {
-	int eflags;
-	eflags = io_load_eflags();	// 读取eflags寄存器中的对应硬件信息
-	io_cli();	// 关中断 清理IF位
-	// 设置定时器信息
-	timerctl.timeout = timeout;
-	timerctl.queue = queue;
-	timerctl.data = data;
-	io_store_eflags(eflags);	// 恢复eflags（恢复IF位）
 }
