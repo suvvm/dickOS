92e9254a3259aca88a06020c03b2093f43154b68
suvvm <915269675@qq.com>
2020-01-31 20:51:14 +0800

    合并缓冲区

--- bootpack.c
+++ bootpack.c
@@ -1,7 +1,7 @@
 /********************************************************************************
 * @File name: bootpack.c
 * @Author: suvvm
-* @Version: 0.2.6
+* @Version: 0.2.7
 * @Date: 2020-01-31
 * @Description: 包含启动后要使用的功能函数
 ********************************************************************************/
@@ -81,7 +81,8 @@ void makeWindow(unsigned char *buf, int width, int height, char *title) {
 **********************************************************/
 void Main(){
 	struct BOOTINFO *binfo;
-	char s[40], keyb[32], mouseb[128], timerb[8];	// s保存要输出的变量信息 keyb键盘缓冲区 mouseb鼠标缓冲区 timerb定时器缓冲区
+	char s[40];
+	int	buf[128];	// s保存要输出的变量信息 buf为总缓冲区
 	int mx, my, bufval, count = 0; //鼠标x轴位置 鼠标y轴位置 要显示的缓冲区信息
 	struct MouseDec mdec;	// 保存鼠标信息
 	unsigned int memtotal;
@@ -89,48 +90,42 @@ void Main(){
 	struct SHTCTL *shtctl;	// 图层控制块指针
 	struct SHEET *sheetBack, *sheetMouse, *sheetWin;	// 背景图层 鼠标图层 窗口图层
 	unsigned char *bufBack, bufMouse[256], *bufWin;	// 背景图像缓冲区 鼠标图像缓冲区 窗口图像缓冲区
-	struct QUEUE timerbuf;	// 三个定时器缓冲区
+	struct QUEUE queue;	// 总缓冲区
 	struct TIMER *timer1, *timer2, *timer3;	// 三个定时器指针
-	
 	binfo = (struct BOOTINFO *) ADR_BOOTINFO;	// 获取启动信息
 	
 	initGdtit();	// 初始化GDT IDT
 	init_pic();	// 初始化可编程中断控制器
 	io_sti();	// 解除cpu中断禁止
-	
-	
-	QueueInit(&keybuf, 32, keyb);	//初始化键盘缓冲区队列
-	QueueInit(&mousebuf, 128, mouseb);	// 初始化鼠标缓冲区队列
-	
-	
+		
+	QueueInit(&queue, 128, buf);	// 初始化缓冲区队列
 	
 	initPit();	// 初始化定时器
+	initKeyboard(&queue, 256);	// 初始化键盘控制电路
+	enableMouse(&queue, 512, &mdec);	// 激活鼠标
+	
 	io_out8(PIC0_IMR, 0xf8); // 主PIC IRQ0(定时器) IRQ1（键盘）与IRQ2（从PIC）不被屏蔽(11111000)
 	io_out8(PIC1_IMR, 0xef); // 从PIC IRQ12（鼠标）不被控制(11101111)
 	
-	QueueInit(&timerbuf, 8, timerb);	// 初始化定时器缓冲区队列
 	timer1 = timerAlloc();	// 获取一个可使用的定时器
-	timerInit(timer1, &timerbuf, 10);	// 初始化定时器1
+	timerInit(timer1, &queue, 10);	// 初始化定时器1
 	timerSetTime(timer1, 1000);
 
 	timer2 = timerAlloc();	// 获取一个可使用的定时器
-	timerInit(timer2, &timerbuf, 3);	// 初始化定时器1
+	timerInit(timer2, &queue, 3);	// 初始化定时器1
 	timerSetTime(timer2, 300);
 
 	timer3 = timerAlloc();	// 获取一个可使用的定时器
-	timerInit(timer3, &timerbuf, 1);	// 初始化定时器1
+	timerInit(timer3, &queue, 1);	// 初始化定时器1
 	timerSetTime(timer3, 50);
-	
-	initKeyboard();
-	enableMouse(&mdec);	// 激活鼠标
 	
 	memtotal = memtest(0x00400000, 0xbfffffff);	// 获取内存总和
 	memsegInit(memsegtable);
 	memsegFree(memsegtable, 0x00001000, 0x0009e000);
 	memsegFree(memsegtable, 0x00400000, memtotal - 0x00400000);
 	
-	
 	init_palette();	// 初始化16色调色板
+	
 	shtctl = shtctlInit(memsegtable, binfo->vram, binfo->scrnx, binfo->scrny);
 	sheetBack = sheetAlloc(shtctl);
 	sheetMouse = sheetAlloc(shtctl);
@@ -143,8 +138,6 @@ void Main(){
 	init_GUI(bufBack, binfo->scrnx, binfo->scrny);	// 初始化GUI至bufBack
 	initMouseCursor8(bufMouse, COL8_008484);	// 初始化鼠标至bufMouse
 	makeWindow(bufWin, 160, 52, "counter");	// 显示窗口
-	// putFont8_asc(bufWin, 160, 24, 28, COL8_000000, "Welcome to");
-	// putFont8_asc(bufWin, 160, 24, 44, COL8_000000, "DickOS");
 	sheetSlide(sheetBack, 0, 0);	// 背景图层起始坐标(0,0)
 	// 初始鼠标坐标为屏幕正中
 	mx = (binfo->scrnx - 16) / 2;	// 鼠标x轴位置
@@ -155,48 +148,29 @@ void Main(){
 	sheetUpdown(sheetWin, 1);	// 将窗口图层置于背景之上鼠标之下
 	sheetUpdown(sheetMouse, 2);	// 将鼠标图层置于背景层上方索引1
 	
-	putFont8AscSheet(sheetBack, 30, 32, COL8_FFFFFF, COL8_008484,  "DickOS", 6);
+	putFont8AscSheet(sheetBack, 30, 32, COL8_FFFFFF, COL8_008484,  "DickOS", 6);	// 将DickOS写入背景层
 	sprintf(s, "(%3d, %3d)", mx, my);	// 将鼠标位置存入s
-	putFont8AscSheet(sheetBack, 0, 0, COL8_FFFFFF, COL8_008484,  s, 10);
+	putFont8AscSheet(sheetBack, 0, 0, COL8_FFFFFF, COL8_008484,  s, 10);	// 将s写入背景层
 	sprintf(s, "memory %dMB free : %dKB", memtest(0x00400000, 0xbfffffff) / (1024 * 1024), memsegTotal(memsegtable) / 1024);	// 将内存信息存入s
-	putFont8AscSheet(sheetBack, 0, 48, COL8_FFFFFF, COL8_008484,  s, 26);
+	putFont8AscSheet(sheetBack, 0, 48, COL8_FFFFFF, COL8_008484,  s, 26);	// 将s写入背景层
 	
-	/*
-	putFont8_asc(bufBack, binfo->scrnx, 30, 32, COL8_FFFFFF, "DickOS");	// 将DickOS写入背景层
-	sprintf(s, "(%d, %d)", mx, my);	// 将鼠标位置存入s
-	putFont8_asc(bufBack, binfo->scrnx, 0, 0, COL8_FFFFFF, s);	// 将s写入背景图层
-	sprintf(s, "memory %dMB free : %dKB", memtest(0x00400000, 0xbfffffff) / (1024 * 1024), memsegTotal(memsegtable) / 1024);	// 将内存信息存入s
-	putFont8_asc(bufBack, binfo->scrnx, 0, 64, COL8_FFFFFF, s);	// 将s写入背景图层
-	sheetRefresh(sheetBack, 0, 0, binfo->scrnx, 64 + 16 + 16 + 76);	// 刷新图层
-	*/
-	//处理键盘与鼠标中断与进入hlt
+	//处理中断与进入hlt
 	for(;;){
 		count++;
 		// sprintf(s, "%010d", timerctl.count);	
-		// putFont8AscSheet(sheetWin, 40, 28, COL8_000000, COL8_C6C6C6,  s, 10);
-		/*
-		boxFill8(bufWin, 160, COL8_C6C6C6, 40, 28, 119, 43);
-		putFont8_asc(bufWin, 160, 40, 28, COL8_000000, s);	// 打印10宽数字
-		sheetRefresh(sheetWin, 40, 28, 120, 44);
-		*/
+		// putFont8AscSheet(sheetWin, 40, 28, COL8_000000, COL8_C6C6C6,  s, 10);	// 显示定时器计时
+
 		io_cli();	// 关中断
-		if(QueueSize(&keybuf) + QueueSize(&mousebuf) + QueueSize(&timerbuf) == 0) {	// 只有在两个缓冲区都没有数据时才能开启中断并进入hlt模式
+		if(QueueSize(&queue) == 0) {	// 只有缓冲区没有数据时才能开启中断并进入hlt模式
 			io_sti();	// 开中断
 		} else {
-			if (QueueSize(&keybuf) != 0) {
-				bufval = QueuePop(&keybuf);
-				io_sti();
+			bufval = QueuePop(&queue);	// 取出缓冲区队列队首数据
+			io_sti();	// 开中断
+			if (256 <= bufval && bufval <= 511) {	// 键盘中断数据
 				sprintf(s, "%02X", bufval);
-				putFont8AscSheet(sheetBack, 0, 16, COL8_FFFFFF, COL8_008484, s, 2);
-				/*
-				boxFill8(bufBack, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
-				putFont8_asc(bufBack, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
-				sheetRefresh(sheetBack, 0, 16, 16, 32);
-				*/
-			} else if (QueueSize(&mousebuf) != 0) {
-				bufval = QueuePop(&mousebuf);
-				io_sti();
-				if (mouseDecode(&mdec, bufval) != 0) {	//完成一波三个字节数据的接收或者出现未知差错
+				putFont8AscSheet(sheetBack, 0, 16, COL8_FFFFFF, COL8_008484, s, 2);	// 将键盘中断信息打印至背景层
+			} else if (512 <= bufval && bufval <= 767) {	// 鼠标中断数据
+				if (mouseDecode(&mdec, bufval - 512) != 0) {	// 完成一波三个字节数据的接收或者出现未知差错
 					sprintf(s, "[lcr %4d %4d]", mdec.x, mdec.y);
 					if ((mdec.btn & 0x01) != 0) {	// 按下左键
 						s[1] = 'L';
@@ -207,14 +181,9 @@ void Main(){
 					if ((mdec.btn & 0x04) != 0) {	// 中间滚轮
 						s[2] = 'C';
 					}
+					putFont8AscSheet(sheetBack, 32, 16, COL8_FFFFFF, COL8_008484, s, 15);	// 在背景层打印鼠标按键信息
 					
-					putFont8AscSheet(sheetBack, 32, 16, COL8_FFFFFF, COL8_008484, s, 15);
-					/*
-					boxFill8(bufBack, binfo->scrnx, COL8_008484, 32, 16, 32 + 15 * 8 - 1, 31);	// 用背景色覆盖原有鼠标信息
-					putFont8_asc(bufBack, binfo->scrnx, 32, 16, COL8_FFFFFF, s);	//打印鼠标信息
 					// 鼠标坐标加上偏移量
-					sheetRefresh(sheetBack, 32, 16, 32 + 15 * 8, 32);
-					*/
 					mx += mdec.x;
 					my += mdec.y;
 					
@@ -229,38 +198,27 @@ void Main(){
 						my = binfo->scrny - 1;
 					
 					sprintf(s, "(%3d, %3d)", mx, my);
-					
-					putFont8AscSheet(sheetBack, 0, 0, COL8_FFFFFF, COL8_008484, s, 10);
-					
-					/*
-					boxFill8(bufBack, binfo->scrnx, COL8_008484, 0, 0, 79, 15);	// 覆盖原有坐标信息
-					putFont8_asc(bufBack, binfo->scrnx, 0, 0, COL8_FFFFFF, s);	// 显示新的坐标信息
-					sheetRefresh(sheetBack, 0, 0, 80, 16);
-					*/
+					putFont8AscSheet(sheetBack, 0, 0, COL8_FFFFFF, COL8_008484, s, 10);	// 在背景层打印鼠标坐标信息
 					sheetSlide(sheetMouse, mx, my);
-				} 
-			} else if (QueueSize(&timerbuf) != 0) {	// 10秒超时显示
-				bufval = QueuePop(&timerbuf);	// 获取定时器缓冲区队列队首数据
-				io_sti();	// 开中断
-				if (bufval == 10) {
-					putFont8AscSheet(sheetBack, 0, 80, COL8_FFFFFF, COL8_008484,  "10[sec]", 7);	
-					sprintf(s, "%010d", count);
-					putFont8AscSheet(sheetWin, 40, 28, COL8_000000, COL8_C6C6C6,  s, 11);
-				} else if (bufval == 3) {
-					putFont8AscSheet(sheetBack, 0, 96, COL8_FFFFFF, COL8_008484,  "3[sec]", 6);	
-					count = 0;
-				} else {
-					if (bufval != 0) {	// 超时信息不为0
-						timerInit(timer3, &timerbuf, 0);	// 超时信息置0
-						boxFill8(bufBack, binfo->scrnx, COL8_FFFFFF, 8, 112, 15, 127);
-					} else {	// 超时信息为0
-						timerInit(timer3, &timerbuf, 1);	// 超时信息置1
-						boxFill8(bufBack, binfo->scrnx, COL8_008484, 8, 112, 15, 127);
-					}
-					timerSetTime(timer3, 50);	// 设置超时时限0.5秒
-					sheetRefresh(sheetBack, 8, 112, 16, 128);
-				}
-			} 
+				} 			
+			} else if (bufval == 10) {	// 10秒定时器中断信息
+				putFont8AscSheet(sheetBack, 0, 80, COL8_FFFFFF, COL8_008484,  "10[sec]", 7);	// 在背景层打印10秒提示	
+				sprintf(s, "%010d", count);
+				putFont8AscSheet(sheetWin, 40, 28, COL8_000000, COL8_C6C6C6,  s, 11);	// 在窗口层打印count的值
+			} else if (bufval == 3) {	// 3秒定时器中断信息
+				putFont8AscSheet(sheetBack, 0, 96, COL8_FFFFFF, COL8_008484,  "3[sec]", 6);	// 在背景层打印3秒提示	
+				count = 0;
+			} else if (bufval == 1) {
+				timerInit(timer3, &queue, 0);	// 超时信息置0
+				boxFill8(bufBack, binfo->scrnx, COL8_FFFFFF, 8, 112, 15, 127);
+				timerSetTime(timer3, 50);	// 设置超时时限0.5秒
+				sheetRefresh(sheetBack, 8, 112, 16, 128);
+			} else if (bufval == 0) {
+				timerInit(timer3, &queue, 1);	// 超时信息置1
+				boxFill8(bufBack, binfo->scrnx, COL8_008484, 8, 112, 15, 127);
+				timerSetTime(timer3, 50);	// 设置超时时限0.5秒
+				sheetRefresh(sheetBack, 8, 112, 16, 128);
+			}
 		}
 	}
 }
